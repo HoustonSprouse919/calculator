a08e982412ca0dbcb49f2776e818c5c160e8483a
HoustonSprouse919 <houstonsprouse@gmail.com>
2022-08-05 15:06:39 -0400

    number input is now working

--- javascript.js
+++ javascript.js
@@ -1,72 +1,66 @@
-const add = function(x,y) {
-	let addition = x + y;
-  return addition;
-};
-
-const subtract = function(x, y) {
-	let subtraction = x-y;
-  return subtraction;
-};
-const divide = function(x,y) {
-    let division = x/y ;
-return division;
-};
-const multiply = function(x,y) {
-    let division = x*y ;
-return division;
-};
-// const sumArray = function(array) {
-// 	let total= 0;
-//   for(var i=0; i<array.length; i++ ){
-//     total += array[i]
-//   }
-//   return total;
-// };
-
-// const multiplyArray = function(array) {
-// 	let total= 1;
-//   for(var i=0; i<array.length; i++ ){
-//     total *= array[i]
-//   }
-//   return total;
-//   //return array.reduce((a, b)=> a*b, 1)
-// };
-
-// const divideArray = function(array) {
-// 	let total= 1;
-//   for(var i=0; i<array.length; i++ ){
-//     total /= array[i]
-//   }
-//   return total;
-//   //return array.reduce((a, b)=> a*b, 1)
-// };
-
-// const power = function(a,b) {
-// 	//return Math.pow(a,b)
-//   let pow = 1;
-//   for(i=0; i< b; i++){
-    
-//     pow *= a;
-//   }
-//   return pow;
-// };
-
-// const factorial = function(num) {
-//   let total = 1;
-//   for(var i=1; i<=num; i++ ){
-//     total *= i;
-//   }
-//   return total;
-// };
-
-function operate(num1, num2, operator){
-    if(operator ===add){
-        return add(num1,num2);
-    } else if(operator ===subtract){
-        return subtract(num1,num2);
-    } else if(operator ===multiply){
-        return multiply(num1,num2);
-} else if(operator ===divide){
-    return divide(num1,num2);
+let displayValue = "";
+function num(num){
+    displayValue = displayValue + num;
+    popDisplay();
+    console.log(displayValue)
 }
+
+function clear(){
+    displayValue = "";
+    popDisplay();
+    console.log(displayValue)
+  }
+function multiply() {
+  
+}
+
+function divide() {
+  
+}
+
+function equal() {
+    popDisplay();
+}
+  function decimal() {
+    displayValue += "1";
+  }
+  function plus() {
+    displayValue += "1";
+  }
+  function minus() {
+    displayValue += "1";
+  }
+function add(x, y) {
+  let addition = x + y;
+  return addition;
+}
+
+function subtract(x, y) {
+  let subtraction = x - y;
+  return subtraction;
+}
+function divide(x, y) {
+  let division = x / y;
+  return division;
+}
+function multiply(x, y) {
+  let division = x * y;
+  return division;
+}
+
+let operation = "";
+function operate(num1, num2, operator) {
+  if (operator === add) {
+    return add(num1, num2);
+  } else if (operator === subtract) {
+    return subtract(num1, num2);
+  } else if (operator === multiply) {
+    return multiply(num1, num2);
+  } else if (operator === divide) {
+    return divide(num1, num2);
+  }
+}
+function popDisplay() {
+  display.textContent =  displayValue;
+
 }
