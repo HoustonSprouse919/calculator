27b40f1e6e67566265ddf4cc05a88f6ab1b1e693
HoustonSprouse919 <houstonsprouse@gmail.com>
2022-08-05 22:41:44 -0400

    equal is now fully working and pop display is also working!

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 let displayValue = ""; //global variable used for displaying and also math
-let operator = ""
-let firstNum = ""
-let op = ""
+let operator = "";
+let firstNum = "";
+let op = "";
 function num(num){ //adds full functionality for pressing all the numbered buttons
     displayValue = displayValue + num;
     popDisplay();
@@ -23,20 +23,27 @@ function signs(sign) {
 
 
 function equal() {
-  let num1 = parseInt(firstNum);  //these two are correctly giving numbers
-  let num2 = parseInt(displayValue);
+ let num1 = parseInt(firstNum);  //these two are correctly giving numbers
+ let num2 = parseInt(displayValue);
+ console.log(num1);
+ console.log(typeof num1);
+ console.log(num2);
+ console.log(typeof num2);
+ console.log(operator);
+ console.log(typeof operator);
+ 
  displayValue = operate(num1,num2,operator);
     popDisplay();
     console.log(displayValue)
 }
-function operate(num1, num2, operator) {
-  if (operator === add) {
+function operate(num1, num2, operator) { //this is used to call the actual operations
+  if (operator == "add") {  //note not having the operation names as strings with "" caused it to not work
     op = add(num1, num2);
-  } else if (operator === subtract) {
+  } else if (operator == "subtract") {
     op = subtract(num1, num2);
-  } else if (operator === multiply) {
+  } else if (operator == "multiply") {
     op = multiply(num1, num2);
-  } else if (operator === divide) {
+  } else if (operator == "divide") {
     op = divide(num1, num2);
   }
   return op;
@@ -64,4 +71,4 @@ function add(x, y) {
     let division = x * y;
     return division;
   }
-  //console.log(operate(7, 3, plus));
+  console.log(operate(7, 3, subtract));
