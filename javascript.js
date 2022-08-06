a258fd55e587df09501d1e593485dfe9660ae944
HoustonSprouse919 <houstonsprouse@gmail.com>
2022-08-06 16:39:43 -0400

    added not having to clear after every number

--- javascript.js
+++ javascript.js
@@ -56,7 +56,9 @@ function equal() {
  displayValue = operation.toString();
  operator = "";
  numOp = 0;
+ firstNum = displayValue;
  popDisplay();
+ displayValue = "";
 }
 }
 function operate(num1, num2, operator) { //this is used to call the actual operations
